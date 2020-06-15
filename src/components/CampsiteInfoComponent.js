e021f098d859a51fc2ba3b4f568e73ca01850fe0
MarquesDragan <47039402+MarquesDragan@users.noreply.github.com>
2020-06-15 18:05:48 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -24,9 +24,9 @@ class CommentForm extends Component {
         });
     }
 
-    handleSubmit(event){
-        alert(`Current State is:`+JSON.stringify(event));
+    handleSubmit(values) {
         this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render() {
@@ -106,7 +106,7 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
     if (comments) {
     return (
         <div className = "col-md-5 and m-1">
@@ -123,7 +123,7 @@ function RenderComments({comments}) {
                 )
             }
             )}
-            <CommentForm />
+            <CommentForm campsiteId={campsiteId} addComment={addComment} />
         </div>
     );
         }
@@ -146,7 +146,11 @@ function CampsiteInfo(props) {
                 </div>                
                 <div className = "row">
                     <RenderCampsite campsite = {props.campsite}/>
-                    <RenderComments comments = {props.comments} />
+                    <RenderComments 
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
